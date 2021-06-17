28461732d9f9ee1ca0d49d817e35f070478dffe0
Your Name <rakhmedov18@gmail.com>
2021-06-17 18:03:36 +0300

    fixed decathlon parser

---
+++ parser_decathlon/settings.py
@@ -0,0 +1,8 @@
+HOST = 'https://www.decathlon.ru/'
+URL = 'https://www.decathlon.ru/C-39446-velosport/'
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,\
+    application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.99',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
+    Chrome/91.0.4472.101 Safari/537.36 Edg/91.0.864.48'
+}
